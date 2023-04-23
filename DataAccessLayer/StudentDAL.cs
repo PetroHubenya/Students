f9c20d35b9f8280964b3664a025da2511ce3b00d
Petro Hubenya <p.hubenya@gmail.com>
2023-04-23 15:41:58 +0200

    Extract data from SQL using method in StudentDAL.

---
+++ DataAccessLayer/StudentDAL.cs
@@ -0,0 +1,55 @@
+﻿using CommonLayer;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    internal class StudentDAL
+    {
+        public List<StudentEntity> students = new();
+        public void Students()
+        {
+            try
+            {
+                // Create connection string.
+                string connectionString = "Data Source=DESKTOP-I1QO562;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+                
+                // Create connection to the SQL database.
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Create SQL query.
+                    string sqlQuery = "SELECT * FROM Student;";
+
+                    // Create SQL command.
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection)) 
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                StudentEntity student = new StudentEntity();
+                                student.Id = reader.GetInt32(0);
+                                student.Name = reader.GetString(1);
+                                student.Email = reader.GetString(2);
+                                student.Age = reader.GetInt32(3);
+
+                                students.Add(student);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
