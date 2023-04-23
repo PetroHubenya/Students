00e497cd52c097a089f3c77fc5665daf117d0a9b
Petro Hubenya <p.hubenya@gmail.com>
2023-04-23 12:34:30 +0200

    Add StudentContext.

---
+++ DataAccessLayer/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using CommonLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    internal class StudentContext : DbContext
+    {
+        public DbSet<StudentEntity> Students { get; set; }
+    }
+}
