b2d0b9cae82bae84002c752801c685324d0f7da4
Petro Hubenya <p.hubenya@gmail.com>
2023-04-23 19:15:18 +0200

    Index.cshtml.cs updated

--- PresentationLayer/Pages/Index.cshtml.cs
+++ PresentationLayer/Pages/Index.cshtml.cs
@@ -9,18 +9,37 @@ namespace PresentationLayer.Pages
 {
     public class IndexModel : PageModel
     {
+        /*
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
+        */
 
+
+        /*
         public List<StudentEntity> Students { get; set; }
 
         public void OnGet(StudentBLL studentBLL)
         {
             Students = studentBLL.GetStudentBelow3();
+        }
+        */
+
+        private readonly StudentBLL _studentBLL;
+
+        public IndexModel(StudentBLL studentBLL)
+        {
+            _studentBLL = studentBLL;
+        }
+
+        public List<StudentEntity> Students { get; set; }
+
+        public void OnGet()
+        {
+            Students = _studentBLL.GetStudentBelow3();
         }
     }
 }
