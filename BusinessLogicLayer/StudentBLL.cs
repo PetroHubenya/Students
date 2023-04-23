8e077deaff7cb8871b0cb12d3b5a8b4a8a55f3e3
Petro Hubenya <p.hubenya@gmail.com>
2023-04-23 09:49:50 +0200

    Create StudentBLL file/class. Create list of students in it.

---
+++ BusinessLogicLayer/StudentBLL.cs
@@ -0,0 +1,40 @@
+﻿using CommonLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    internal class StudentBLL
+    {
+        List<StudentEntity> students = new()
+        {
+            new StudentEntity()
+            {
+                Id = 1,
+                Name = "Name1",
+                Email = "name1@gmail.com",
+                Age = 20
+            },
+
+            new StudentEntity()
+            {
+                Id = 2,
+                Name = "Name2",
+                Email = "name2@gmail.com",
+                Age = 20
+            },
+
+            new StudentEntity()
+            {
+                Id = 3,
+                Name = "Name3",
+                Email = "name3@gmail.com",
+                Age = 20
+            }
+        };
+
+    }
+}
