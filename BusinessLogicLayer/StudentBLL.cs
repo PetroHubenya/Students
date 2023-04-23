e83b9067330b844eaf004aa6c967dd9f3bc50fb4
Petro Hubenya <p.hubenya@gmail.com>
2023-04-23 10:31:01 +0200

    Apply business logic in StudentBLL file.

--- BusinessLogicLayer/StudentBLL.cs
+++ BusinessLogicLayer/StudentBLL.cs
@@ -7,41 +7,39 @@ using System.Threading.Tasks;
 
 namespace BusinessLogicLayer
 {
-    internal class StudentBLL
+    public class StudentBLL
     {
         readonly List<StudentEntity> students = new()
-                 {
-                    new StudentEntity()
-                    {
-                        Id = 1,
-                        Name = "Name1",
-                        Email = "name1@gmail.com",
-                        Age = 20
-                    },
+        {
+            new StudentEntity()
+            {
+                Id = 1,
+                Name = "Name1",
+                Email = "name1@gmail.com",
+                Age = 20
+            },
 
-                    new StudentEntity()
-                    {
-                        Id = 2,
-                        Name = "Name2",
-                        Email = "name2@gmail.com",
-                        Age = 20
-                    },
+            new StudentEntity()
+            {
+                Id = 2,
+                Name = "Name2",
+                Email = "name2@gmail.com",
+                Age = 20
+            },
 
-                    new StudentEntity()
-                    {
-                        Id = 3,
-                        Name = "Name3",
-                        Email = "name3@gmail.com",
-                        Age = 20
-                    }
-                 };
+            new StudentEntity()
+            {
+                Id = 3,
+                Name = "Name3",
+                Email = "name3@gmail.com",
+                Age = 20
+            }
+        };
 
         public List<StudentEntity> GetStudentBelow3()
         {
-            List<StudentEntity> studentsBelow3 = new();
-            studentsBelow3 = (List<StudentEntity>)(from s in students
-                             where s.Id < 3
-                             select s);
+            List<StudentEntity> studentsBelow3 = students.Where(s => s.Id < 3).ToList();
+            
             return studentsBelow3;
         }
 
--- PresentationLayer/Pages/Index.cshtml.cs
+++ PresentationLayer/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using CommonLayer;
+using BusinessLogicLayer;
 using System.Runtime.InteropServices;
 
 namespace PresentationLayer.Pages
@@ -16,38 +17,9 @@ namespace PresentationLayer.Pages
 
         public List<StudentEntity> Students { get; set; }
 
-        public void OnGet()
+        public void OnGet(StudentBLL studentBLL)
         {
-            List<StudentEntity> students = new()
-            {
-                new StudentEntity()
-                {
-                    Id = 1,
-                    Name = "Name1",
-                    Email = "name1@gmail.com",
-                    Age = 20
-                },
-
-                new StudentEntity()
-                {
-                    Id = 2,
-                    Name = "Name2",
-                    Email = "name2@gmail.com",
-                    Age = 20
-                },
-
-                new StudentEntity()
-                {
-                    Id = 3,
-                    Name = "Name3",
-                    Email = "name3@gmail.com",
-                    Age = 20
-                }
-            };
-
-            Students = students;
-
-
+            Students = studentBLL.GetStudentBelow3();
         }
     }
 }
