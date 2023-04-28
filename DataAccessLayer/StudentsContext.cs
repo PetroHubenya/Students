c85451ff306c0c1294b9537d3d7dbde801f58959
Petro Hubenya <p.hubenya@gmail.com>
2023-04-28 16:15:56 +0200

    Add connection string to appsettings.json

---
+++ DataAccessLayer/StudentsContext.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class StudentsContext : DbContext
+    {
+        public StudentsContext(DbContextOptions<StudentsContext> options) : base(options) { }
+        public DbSet<StudentEntity> Students { get; set; }
+    }
+}
